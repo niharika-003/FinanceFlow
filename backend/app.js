ebb19184a3bbfcaab2340dfc8f4cc6077f781093
niharika-003 <gangalaniharika@gmail.com>
2026-04-17 11:26:28 +0530

    Updated changes

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,11 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
